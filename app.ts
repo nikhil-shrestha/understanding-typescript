0ab04cac1c22851fe8927b09907514ba5dd65909
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-29 18:07:36 +0545

    intro to typescript objects

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect Input!');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Nikhil',
+  age: 25
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
