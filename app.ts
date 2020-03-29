7ac28d1f192f4fbd454a4a1bcce20bf7075ff0af
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-29 18:16:05 +0545

    intro to array

--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: 'Nikhil',
+//   age: 25,
+// };
+const person = {
   name: 'Nikhil',
-  age: 25
+  age: 25,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivitites: string[];
+favoriteActivitites = ['Sports', 1];
+
 console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
