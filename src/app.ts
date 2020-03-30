caefe097b21333fb626bb5d2f0ea7ba6bb0b98ab
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-30 11:47:26 +0545

    intro to classes and inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,65 @@
-const button = document.querySelector('button')!;
+class Department {
+  // private id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-});
+  constructor(private id: string, public name: string) {
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'IT');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment('d1', ['Nikhil']);
+
+it.addEmployee('Nikhil');
+it.addEmployee('Akhil');
+
+// it.employees[2] = 'Punam';
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInfo();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReport('Something went wrong...');
+accounting.printReports();
+
+// const itCopy = { name: 's', describe: it.describe };
+// itCopy.describe();
