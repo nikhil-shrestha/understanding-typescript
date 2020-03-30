400e30fc03dc82f3a2c2c9577f127248989752a9
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-30 13:48:22 +0545

    readonly and extend interface

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-interface Greetable {
-  name: string;
-
+interface Named {
+  readonly name: string;
+}
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
@@ -20,5 +21,6 @@ class Person implements Greetable {
 let user1: Greetable;
 
 user1 = new Person('Max');
+// user1.name = 'manu';
 
 user1.greet('Hi there - I am');
