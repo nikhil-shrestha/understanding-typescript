8ee63a0a8e915a767bea98d4936d2c2449dd1fcd
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-30 13:59:58 +0545

    optional parameters and properties

--- src/app.ts
+++ src/app.ts
@@ -10,18 +10,21 @@ add = (n1: number, n2: number) => {
 };
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
@@ -31,7 +34,7 @@ class Person implements Greetable {
 
 let user1: Greetable;
 
-user1 = new Person('Max');
+user1 = new Person();
 // user1.name = 'manu';
 
 user1.greet('Hi there - I am');
