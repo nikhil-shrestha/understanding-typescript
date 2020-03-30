b5632a8e4424d4eefdfef60b0ceb9481932c62c0
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-30 20:56:36 +0545

    generics constraints

--- src/app.ts
+++ src/app.ts
@@ -11,10 +11,9 @@
 //   // data.split(' ');
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-const mergedObj2 = merge({ name: 'Max' }, { age: 30 });
-console.log(mergedObj.age);
+console.log(mergedObj);
