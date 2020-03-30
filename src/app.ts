e81268b33a5d5b0fff6dc0d1b1a463461814f57b
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-30 21:16:50 +0545

    generic classes & primitive and reference values

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,37 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: 'Nikhil' }, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) == -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objectStorage = new DataStorage<object>();
+// const maxObj = { name: 'Max' };
+// objectStorage.addItem(maxObj);
+// objectStorage.addItem({ name: 'Manu' });
+// objectStorage.removeItem(maxObj);
+// console.log(objectStorage.getItems());
