0d3db54b2f5285cfaba936c6082462ed9ffbfe60
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-31 07:52:20 +0545

    adding multiple decorators

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-function Logger(constructor: Function): void {
-  console.log('Logging...');
-  console.log(constructor);
+function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function withTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  return function(constructor: any) {
+    console.log('Rendering template..');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+@Logger('Loggin')
+@withTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Max';
 
