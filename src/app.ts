aedb062cc1286ae7d11fb997ba399862c5e76beb
illusionist <nikhil.shrestha1995@gmail.com>
2020-03-31 07:35:28 +0545

    intro to decorator

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,16 @@
-// const names: Array<string | number> = ['Max', 'Manu']; // string[]
-// name[0].split(' ');
-
-// const promise: Promise<number> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10);
-//   }, 1500);
-// });
-
-// promise.then(data => {
-//   // data.split(' ');
-// });
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function Logger(constructor: Function): void {
+  console.log('Logging...');
+  console.log(constructor);
 }
 
-const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObj);
+@Logger
+class Person {
+  name = 'Max';
 
-interface Lengthy {
-  length: number;
-}
-
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let descriptionText = 'Got no value.';
-  if (element.length === 1) {
-    descriptionText = 'Got 1 element.';
-  } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.';
-  }
-  return [element, descriptionText];
-}
-
-console.log(countAndDescribe(['Sports', 'Cooking']));
-
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return 'Value: ' + obj[key];
-}
-
-extractAndConvert({ name: 'Nikhil' }, 'name');
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) == -1) {
-      return;
-    }
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
+  constructor() {
+    console.log('Creating person object...');
   }
 }
 
-const textStorage = new DataStorage<string>();
-textStorage.addItem('Max');
-textStorage.addItem('Manu');
-textStorage.removeItem('Max');
-console.log(textStorage.getItems());
-
-const numberStorage = new DataStorage<number>();
-
-// const objectStorage = new DataStorage<object>();
-// const maxObj = { name: 'Max' };
-// objectStorage.addItem(maxObj);
-// objectStorage.addItem({ name: 'Manu' });
-// objectStorage.removeItem(maxObj);
-// console.log(objectStorage.getItems());
-
-interface CourseGoal {
-  title: string;
-  description: string;
-  completeUntil: Date;
-}
-
-function createCourse(
-  title: string,
-  description: string,
-  date: Date
-): CourseGoal {
-  let courseGoal: Partial<CourseGoal> = {};
-  courseGoal.title = title;
-  courseGoal.description = description;
-  courseGoal.completeUntil = date;
-
-  return courseGoal as CourseGoal;
-}
-
-const names: Readonly<string[]> = ['Max', 'Manu'];
-// names.push('Anna');
-// names.pop();
+const pers = new Person();
+console.log(pers);
