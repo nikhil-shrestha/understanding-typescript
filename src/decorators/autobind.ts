808da71a26746d9c5d8d2f2829a810788c4565d9
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-13 16:22:29 +0545

    using ES modules

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // autobind decorator
-  export function autobind(
-    _target: any,
-    _methodName: string,
-    descriptor: TypedPropertyDescriptor<any>
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: TypedPropertyDescriptor<any> = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// autobind decorator
+export function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: TypedPropertyDescriptor<any>
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: TypedPropertyDescriptor<any> = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
