96be2e7842acc89802e7a0839fa48539f4b580b3
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-13 18:20:29 +0545

    class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-declare var GLOBAL: any;
+import { Product } from './product.model';
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 12.99 },
+];
 
-console.log(GLOBAL);
+// const p1 = new Product('A Book', 12.99);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
