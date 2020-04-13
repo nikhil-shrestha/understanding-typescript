7c3e4388c3d31241831c8b5a14c3694abe448046
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-13 18:27:11 +0545

    embracing class-validator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
+
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { Product } from './product.model';
 
@@ -7,6 +9,16 @@ const products = [
   { title: 'A Carpet', price: 29.99 },
   { title: 'A Book', price: 12.99 },
 ];
+
+const newProduct = new Product('', -23);
+validate(newProduct).then((errors) => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(newProduct.getInformation());
+  }
+});
 
 // const p1 = new Product('A Book', 12.99);
 
