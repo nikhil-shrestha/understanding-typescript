3e6ef2f1c29c7801f925f9c43735275cb565a819
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-13 20:01:37 +0545

    axios to fetch coordinates for input address

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,38 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyBVXJsJxO5qKdlT6IH3DoKW4vZksR4zNWg';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   // send this to GOOGLE's API
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}
+  `
+    )
+    .then(({ data }) => {
+      if (data.status !== 'OK') {
+        throw new Error('Could not fetch location');
+      }
+      const coordinates = data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler)!;
